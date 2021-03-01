9f98c377a4216c706254919adef61619dee47b74
sam55silver <sam55silver@users.noreply.github.com>
2021-03-01 19:11:56 -0400

    Added the start to sorting files and fun stuff!

--- MoveFiles.py
+++ MoveFiles.py
@@ -3,6 +3,7 @@ import os
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
+# initialize event handler
 if __name__ == "__main__":
     patterns = "*"
     ignore_patterns = ""
@@ -12,28 +13,46 @@ if __name__ == "__main__":
         patterns, ignore_patterns, ignore_directories, case_sensitive)
 
 
+# If Folder To Track is added to... do the following
 def on_modified(event):
+    # Obtain the filename of file being added
     filename = event.src_path.replace(folder_to_track, "")
-    destination = folder_documents + filename
+
+    # Look at the extension the file has and assign appropriate destination
+    if filename.lower().endswith('.txt'):
+        destination = folder_documents + filename
+    elif filename.lower().endswith('.jpg'):
+        destination = folder_images + filename
+    else:
+        destination = folder_other + filename
+
+    # Move to the destination
     os.rename(event.src_path, destination)
     print(f"Moved {filename} to {destination}")
 
 
+# Add on_modified to the list of events to look out for
 my_event_handler.on_modified = on_modified
 
+# Folder destinations
 folder_to_track = "D:\\Development\\Test\\myFolder"
 folder_images = "D:\\Development\\Test\\images"
 folder_documents = "D:\\Development\\Test\\documents"
+folder_other = "D:\\Development\\Test\\other"
 
+# Setup observer with the event handler and folder to watch
 go_recursively = True
 my_observer = Observer()
 my_observer.schedule(my_event_handler, folder_to_track,
                      recursive=go_recursively)
 
+# Start up observer
 my_observer.start()
+print(f"I have got my eyes on {folder_to_track}...\n")
 try:
     while True:
         time.sleep(1)
-except KeyboardInterrupt:
+except KeyboardInterrupt:  # Stop program with keyboard interruption
+    print("So long!")
     my_observer.stop()
     my_observer.join()
