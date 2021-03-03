87d34d237aefc1085c0140e80325bca4fc4a0648
sam55silver <sam55silver@gmail.com>
2021-03-03 10:06:37 -0400

    Final Version...

--- MoveFiles.py
+++ MoveFiles.py
@@ -1,87 +1,91 @@
-import time
 import os
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
-
-# Retrive users destination for folders
-folder_to_track = input("What path am I tracking today: ")
-# Debug purposes... Not trying to enter every time
-# Add a config file later
-if folder_to_track == 'mac':
-    folder_to_track = '/Users/sam/Down'
-elif folder_to_track == 'win':
-    folder_to_track = 'D:/Development/Test/myFolder'
-
-# Destination folders
-folder_names = ['other', 'images', 'documents']
-# Create destinations if they do not exist
-for names in folder_names:
-    path = folder_to_track + '/' + names
-    if not(os.path.exists(path)):
-        print(f"{path} does not exist... creating it.")
-        os.mkdir(path)
-    else:
-        print(f"{path} exists")  # DEBUG PURPOSE GET RID
-
-# initialize event handler
-if __name__ == "__main__":
-    patterns = "*"
-    ignore_patterns = ""
-    ignore_directories = False
-    case_sensitive = False
-    my_event_handler = PatternMatchingEventHandler(
-        patterns, ignore_patterns, ignore_directories, case_sensitive)
 
 
-def on_created(event):
-    # Obtain the filename of file being added
-    filename = event.src_path.replace(folder_to_track, "")
-    print(f"created an item {event.src_path}")
-    path = event.src_path
+def check_config():
+    # Opens config to get folder to track
+    if os.path.exists("moveFilesConfig.txt"):
+        file = open("moveFilesConfig.txt", "r")
+        folder_to_track = file.read()
+        file.close()
+    else:  # If file does not exist then make one!
+        print("No config file found...")
+        folder_to_track = input(
+            "What path would you like me to track from here on out: ")
 
+        while not(os.path.exists(folder_to_track)):  # Make sure the entered dir exists
+            folder_to_track = input(
+                "That path does not exist... please re-enter:")
+
+        print("Creating config...\n")
+
+        file = open("moveFilesConfig.txt", "w")
+        file.write(folder_to_track)
+        file.close
+    return folder_to_track
+
+
+def sort_files(folder_to_track):
+    # Loop through array of directory and sort each file
+    filesMoved = 0
+    for file in os.listdir(folder_to_track):
+        if file in folder_names:  # Check if its looking at one of the folders we have created above
+            continue
+        destination = check_file_copies(check_file_extension(file), file)
+        print(f"Moving {file} to {destination} \n")
+        os.rename(folder_to_track + "/" + file, destination)
+        filesMoved += 1
+    print(f"Moved {str(filesMoved)} files\nSo long!")
+
+
+def check_file_extension(filename):
     # Look at the extension the file has and assign appropriate destination
-    if filename.lower().endswith('.txt'):
-        destination = folder_to_track + "/" + folder_names[2]
-    elif filename.lower().endswith('.jpg'):
-        destination = folder_to_track + "/" + folder_names[1]
-    else:
-        destination = folder_to_track + "/" + folder_names[0]
+    if filename.lower().endswith('.mp3'):  # Music
+        return folder_to_track + "/" + folder_names[6]
 
+    elif filename.lower().endswith(('.xlsx', '.csv')):  # Spread Sheets
+        return folder_to_track + "/" + folder_names[5]
+
+    elif filename.lower().endswith(('.mov', '.avi', '.mp4')):  # Videos
+        return folder_to_track + "/" + folder_names[4]
+
+    elif filename.lower().endswith(('.pptx', '.ppt')):  # Power points
+        return folder_to_track + "/" + folder_names[3]
+
+    elif filename.lower().endswith(('.txt', '.pdf', '.docx')):  # Documents
+        return folder_to_track + "/" + folder_names[2]
+
+    elif filename.lower().endswith(('.jpg', '.png', '.heic', '.jpeg')):  # Images
+        return folder_to_track + "/" + folder_names[1]
+
+    else:  # Other
+        return folder_to_track + "/" + folder_names[0]
+
+
+def check_file_copies(destination, filename):
     # Loop through destination to see if there are any copies
     index = 0
-    while os.path.exists(destination + filename):
+    while os.path.exists(destination + "/" + filename):
         filename, extension = os.path.splitext(filename)
         if index == 0:
             filename = filename + "(0)" + extension
         else:
             filename = filename.replace(filename[-2], str(index)) + extension
         index += 1
-
-    try:
-        os.rename(path, destination + filename)
-    except FileNotFoundError:  # If we are faced with no file make sure it does not crash file
-        print("You are probs copying and pasting... do not do that")
-
-    # Move to the destination
-    print(f"Moved {filename} to {destination}\n")
+    return destination + "/" + filename
 
 
-# Look out for when a file is created in a folder, if so do function on_created
-my_event_handler.on_created = on_created
+# Check/Check config to retrive trackable path
+folder_to_track = check_config()
 
-# Setup observer with the event handler and folder to watch
-go_recursively = True
-my_observer = Observer()
-my_observer.schedule(my_event_handler, folder_to_track,
-                     recursive=go_recursively)
+# Destination folders
+folder_names = ['other', 'images', 'documents',
+                'powerpoints', 'videos', 'spreadsheets', 'audio']
+# Create destinations if they do not exist
+for names in folder_names:
+    path = folder_to_track + '/' + names
+    if not(os.path.exists(path)):
+        print(f"{path} does not exist... creating it.")
+        os.mkdir(path)
 
-# Start up observer
-my_observer.start()
-print(f"I have got my eyes on {folder_to_track}...\n")
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:  # Stop program with keyboard interruption
-    print("\nSo long!")
-    my_observer.stop()
-    my_observer.join()
+# Sort files in trackable path into individual files
+sort_files(folder_to_track)
